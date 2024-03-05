985de50e7693c3d0a4e843e30081f6083cdb3952
RoshanAditya27 <roshanaditya.m2019@vitstudent.ac.in>
2024-03-05 09:33:02 +0530

    Assignment week 4 Spring security

---
+++ Assignment_week_4/springbootsecurity/src/test/java/com/hexaware/springsecurity/SpringbootsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexaware.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
